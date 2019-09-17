f53e0379ef1d5375109a5ae254f08e0a5704b32e
alumnos <alumnos@7118-14>
2019-09-17 20:28:40 -0300

    Se agregan mas test

--- src/Controllers/CalculadoraTest.java
+++ src/Controllers/CalculadoraTest.java
@@ -20,11 +20,17 @@ class CalculadoraTest {
 	}
 	
 	@Test
-	public void TestSumaConNegativos() {
+	public void testSumaConNegativos() {
 		Calculadora calc = new Calculadora(-2,2,"+");
 		assertTrue(calc.DevolverOperacion().equals("0.0"));
 	}
-
+	
+	@Test
+	public void testSumaConDecimalesNegativos() {
+		Calculadora calc = new Calculadora(-2.6,2.3,"+");
+		assertTrue(calc.DevolverOperacion().equals("-0.30000000000000027"));
+	}
+	
 	@Test
 	public void testResta() {
 		Calculadora calc = new Calculadora(3,2,"-");
@@ -43,18 +49,6 @@ class CalculadoraTest {
 		assertTrue(calc.DevolverOperacion().equals("2.0"));
 	}
 	
-	/*@Test
-	public void testDivisionConDecimales() {
-		Calculadora calc = new Calculadora(6.3,2.3,"/");
-		assertTrue(calc.DevolverOperacion().equals("2,739130434782609"));
-	}*/
-	
-	/*@Test
-	public void TestDivisionConNegativos() {
-		Calculadora calc = new Calculadora(-3.3,2.5,"*");
-		assertTrue(calc.DevolverOperacion().equals("-8.25"));
-	}*/
-
 	@Test
 	public void testMultiplicacion() {
 		Calculadora calc = new Calculadora(5,5,"*");
