6850ad8813166ebd6dddcb27cbe8b85b2fddeafa
alumnos <alumnos@7118-14>
2019-09-17 20:21:12 -0300

    Se agrego que el cuadro no se pueda expander ni contraer

--- src/View/CalculadoraView.java
+++ src/View/CalculadoraView.java
@@ -11,12 +11,9 @@ import java.awt.GridLayout;
 public class CalculadoraView {
 
 	private JFrame frame;
-	private Botones botones;
+	private Botones botones; 
 	private PantallaCalculadora pantalla;
 
-	/**
-	 * Launch the application.
-	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -30,25 +27,19 @@ public class CalculadoraView {
 		});
 	}
 
-	/**
-	 * Create the application.
-	 */
 	public CalculadoraView() {
 		initialize();
 	}
 
-	/**
-	 * Initialize the contents of the frame.
-	 */
 	private void initialize() {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 300, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		frame.setTitle("Calculadora Magica");
+		frame.setResizable(false);
 
 		pantalla = new PantallaCalculadora(frame);
 		botones = new Botones(frame, pantalla);
-
 	}
 }
